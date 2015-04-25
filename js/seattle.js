d5d1f30526e42852ad703db95858b6efb456e397
John Rood <mr.john.rood@gmail.com>
2015-04-25 15:04:37 -0500

    lots of development

---
+++ js/seattle.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
